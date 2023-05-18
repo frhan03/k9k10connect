e59e1720777c79d42fea6143d6d9b728cbf18dc5
NazihahSurati <nazihahn3@gmail.com>
2023-05-18 22:46:37 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:k9k10connect/homepage.dart';
 import 'package:k9k10connect/screens/signin_screen.dart';
 import 'package:k9k10connect/staffhomepage.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  // initialise app based on platform- web or mobile
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBRffTpq-OjnRUduFN-OtmnpnqvHkkv-f4",
+          authDomain: "collegeconnect-262df.firebaseapp.com",
+          projectId: "collegeconnect-262df",
+          storageBucket: "collegeconnect-262df.appspot.com",
+          messagingSenderId: "463624953126",
+          appId: "1:463624953126:web:66c6dc0290298e4e79f5e7"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -19,7 +35,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: const SignInScreen(),
