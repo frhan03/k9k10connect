ca4df6dc84618d22f8cc8238e8c67bfc6a675628
NazihahSurati <nazihahn3@gmail.com>
2023-06-11 14:03:10 +0800

    Update view and delete function

--- lib/staff_pages/status_staff.dart
+++ lib/staff_pages/status_staff.dart
@@ -2,194 +2,167 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:k9k10connect/drawer.dart';
 
-class StatusStaffPage extends StatelessWidget{
-  const StatusStaffPage({super.key});
+class StatusStaffPage extends StatefulWidget {
+  final Map<String, dynamic> report;
+  final String displayName;
+
+  const StatusStaffPage({
+    required this.report,
+    required this.displayName,
+  });
 
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          colorSchemeSeed: Color(0xFFD7CCC8),
-          useMaterial3: true
-      ),
-      home: const MyStatusPage(),
+  _StatusStaffPageState createState() => _StatusStaffPageState();
+}
+
+class _StatusStaffPageState extends State<StatusStaffPage> {
+  late TextEditingController dateController;
+
+  @override
+  void initState() {
+    super.initState();
+    dateController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    dateController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (pickedDate != null) {
+      String formattedDate = DateFormat('dd-MM-yyyy').format(pickedDate);
+      setState(() {
+        dateController.text = formattedDate;
+      });
+    }
+  }
+
+ void _submitStatus() {
+  if (widget.report['status'] != 'Resolved') {
+    // Perform action on button click
+    String selectedDate = dateController.text;
+    String status = 'In-action'; // Change the status to 'In-action'
+    // Update the status in the report
+    widget.report['status'] = status;
+    // You can perform any additional logic or API calls here
+    Navigator.of(context).pop(widget.report); // Return the updated report to the previous page
+  } else {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Invalid Action'),
+          content: Text('The status is already "Resolved".'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
 
-class MyStatusPage extends StatefulWidget{
-  const MyStatusPage({super.key});
-
-  @override
-  State<MyStatusPage> createState()=> _MyStatusPageState();
-}
-
-class _MyStatusPageState extends State<MyStatusPage>{
-  TextEditingController dateController = TextEditingController();
-
-  @override
-  void initState(){
-    super.initState();
-    dateController.text = "";
-  }
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: _buildAppBar(),
-      drawer: MyDrawer(),
-      body: Column(
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: _buildAppBar(),
+    drawer: MyDrawer(),
+    body: Padding(
+      padding: EdgeInsets.all(15.0),
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Container(
-              alignment: Alignment(1, 1),
-              child: Column(
-                children: [
-                  Padding(padding: EdgeInsets.only(top: 20.0)),
-                  ListTile(
-                    title: Text('Name: '),
-                  ),
-                  ListTile(
-                    title: Text('Location: '),
-                  ),
-                  ListTile(
-                    title: Text('Civil Damage: '),
-                  ),
-                  ListTile(
-                    title: Text('Description: '),
-                  ),
-                  ListTile(
-                    title: Text('Status: Pending'),
-                  ),
-                  ListTile(
-                    title: Text('In action: '),
-                  ),
-                  SizedBox(
-                    child: TextField(
-                      controller: dateController,
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.calendar_today),
-                          labelText: "Enter Date"
-                      ),
-                      readOnly: true, // when true user cannot edit text
-                      onTap: () async {
-                        DateTime? pickedDate = await showDatePicker(
-                          context: context,
-                          initialDate: DateTime.now(), //get today's date
-                          firstDate: DateTime.now(), // not to allow to choose before today.
-                          lastDate: DateTime(2101),
-                          builder: (context, child) {
-                            return Theme(
-                              data: Theme.of(context).copyWith(
-                                colorScheme: ColorScheme.light(
-                                  primary: Color(0xFFD7CCC8),
-                                  onPrimary: Colors.black,
-                                  onSurface: Colors.black,
-                                ),
-                                textButtonTheme: TextButtonThemeData(
-                                    style: TextButton.styleFrom(
-                                      primary: Colors.black,
-                                    )
-                                ),
-                              ),
-                              child: child!,
-                            );
-                          },
-                        );
-
-                        if(pickedDate != null){
-                          print(pickedDate); //get the picked date in the format => 2022-07-04 00:00:00.000
-                          String formattedDate = DateFormat('dd-MM-yyyy').format(pickedDate);
-                          print(formattedDate); //formatted date output using intl package =>  16-03-2023
-
-                          setState(() {
-                            dateController.text = formattedDate;
-                          });
-                        } else{
-                          print("Date is not selected");
-                        }
-                      },
+          Container(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                SizedBox(height: 20.0),
+                ListTile(
+                  title: Text('Name: ${widget.displayName}'),
+                ),
+                ListTile(
+                  title: Text('Location: ${widget.report['location']}'),
+                ),
+                ListTile(
+                  title: Text('Civil Damage: ${widget.report['category']}'),
+                ),
+                ListTile(
+                  title: Text('Description: ${widget.report['description']}'),
+                ),
+                ListTile(
+                  title: Text('Status: ${widget.report['status']}'),
+                ),
+                ListTile(
+                  title: Text('In action: ${widget.report['inAction']}'),
+                ),
+                SizedBox(
+                  child: TextField(
+                    controller: dateController,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.calendar_today),
+                      labelText: "Enter Date",
                     ),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 80.0)),
-                  ElevatedButton(
-                    onPressed: () async {
-                      showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              backgroundColor: Colors.blueGrey[50],
-                              title: Container(
-                                width: 250,
-                                height: 50,
-                                padding: EdgeInsets.only(
-                                  left: 90,
-                                  right: 90,
-                                ),
-                                child: Text('Saved'),
-                              ),
-                              actions: <Widget>[
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    ElevatedButton(
-                                      onPressed: (){
-                                        Navigator.pop(context);
-                                      },
-                                      child: Text('OK'),
-                                      style: ElevatedButton.styleFrom(
-                                        primary: Colors.deepOrange[100],
-                                        onPrimary: Colors.black,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(10),
-                                        ),
-                                        padding: EdgeInsets.only(
-                                          left: 50,
-                                          right: 50,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            );
-                          });
+                    readOnly: true,
+                    onTap: () {
+                      _selectDate(context);
                     },
-                    child: Text('Save'),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.deepOrange[100],
-                      onPrimary: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      padding: EdgeInsets.only(
-                        left: 30.0,
-                        right: 30.0,
-                      ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: _submitStatus,
+                      child: Text('Submit'),
                     ),
-                  )
-                ],
-              ),
+                    SizedBox(width: 10.0),
+                    ElevatedButton(
+                      onPressed: _finishStatus,
+                      child: Text('Finish'),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
       ),
-    );
-  }
+    ),
+  );
 }
 
-void _doNothing(){}
+void _finishStatus() {
+  // Perform action on "Finish" button click
+  String status = 'Resolved'; // Change the status to 'Resolved'
+  // Update the status in the report
+  widget.report['status'] = status;
+  // You can perform any additional logic or API calls here
+  Navigator.of(context).pop(widget.report); // Return the updated report to the previous page
+}
 
-AppBar _buildAppBar(){
-  return AppBar(
-    centerTitle: true,
-    title: Text('Status'),
-    actions: <Widget>[
-      IconButton(onPressed: _doNothing, icon: Icon(Icons.notifications)),
-    ],
-  );
+  AppBar _buildAppBar() {
+    return AppBar(
+      centerTitle: true,
+      title: Text('Status'),
+      actions: <Widget>[
+        IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
+      ],
+    );
+  }
 }
